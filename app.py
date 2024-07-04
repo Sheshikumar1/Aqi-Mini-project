7434303aafec5408be823b4e5902880c53ddc6c1
sheshikumar1 <sheshikumarerraboina@gmail.com>
2024-07-04 20:35:51 +0530

    modified

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
 from flask import Flask, render_template, request
-import pickle
 import joblib
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
 
 app = Flask(__name__)
 
 # Load model and encoded values
-model = joblib.load(open("model_small.pkl", 'rb'))
-encoded = joblib.load('label_values')
-
-# Load the label encoder and fit with all city names
+model = joblib.load("model_small.pkl")
 label_encoder = joblib.load('label_encoder.pkl')
 
 @app.route('/')
@@ -31,16 +26,10 @@ def output():
         city = request.form["city"].strip()
         pm25 = float(request.form["pm25"])
         pm10 = float(request.form["pm10"])
-        no = float(request.form["no"])
         no2 = float(request.form["no2"])
-        nox = float(request.form["nox"])
-        nh3 = float(request.form["nh3"])
         co = float(request.form["co"])
         so2 = float(request.form["so2"])
         o3 = float(request.form["o3"])
-        benzene = float(request.form["benzene"])
-        toluene = float(request.form["toluene"])
-        xylene = float(request.form["xylene"])
         date = request.form["date"]
 
         # Ensure the city name is valid
@@ -53,8 +42,8 @@ def output():
         month = int(date.split('-')[1])
 
         # Create a DataFrame for the input features
-        feature_cols = ['City', 'PM2.5', 'PM10', 'NO', 'NO2', 'NOx', 'NH3', 'CO', 'SO2', 'O3', 'Benzene', 'Toluene', 'Xylene', 'Year', 'Month']
-        data = pd.DataFrame([[city_encoded, pm25, pm10, no, no2, nox, nh3, co, so2, o3, benzene, toluene, xylene, year, month]], columns=feature_cols)
+        feature_cols = ['City', 'PM2.5', 'PM10', 'NO2', 'CO', 'SO2', 'O3', 'Year', 'Month']
+        data = pd.DataFrame([[city_encoded, pm25, pm10, no2, co, so2, o3, year, month]], columns=feature_cols)
 
         # Make prediction
         pred = model.predict(data)
